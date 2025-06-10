7553f66555b00d0dfeffc99ac91de6a9ea5ef7e7
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-06-10 11:59:24 +0000

    Bug fixed in getting the events

--- server/routes/get-events.js
+++ server/routes/get-events.js
@@ -5,7 +5,7 @@ import eventModel from '../models/event-model.js';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    await eventModel.find().populate('club').then(evnts => res.json({
+    await eventModel.find().then(evnts => res.json({
         status: 200,
         events: evnts
     })).catch(err => res.json({
